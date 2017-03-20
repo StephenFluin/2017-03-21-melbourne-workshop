05458d463f17333a9085bc17dc85ce8a83bc6e87
Stephen Fluin <stephen.fluin@gmail.com>
2017-03-21 07:30:57 +1100

    Lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ActivityListComponent } from './activity-list.component';
@@ -16,7 +17,12 @@ import { ActivityViewComponent } from './activity-view.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', component: ActivityListComponent },
+      {path: 'about', loadChildren: './about/about.module#AboutModule'},
+      {path: 'submit', loadChildren: './submit/submit.module#SubmitModule'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/submit/submit.module.ts
+++ src/app/submit/submit.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubmitComponent } from './submit.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: SubmitComponent},
+    ])
   ],
   declarations: [SubmitComponent]
 })
